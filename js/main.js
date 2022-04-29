08b2415b965212e7d87fbc52e5c84d5657cdd995
Arnesh Bhattacharya <55632022+Deadbullfrog05@users.noreply.github.com>
2022-04-29 16:25:17 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,146 +1,3 @@
-/*window.addEventListener('load', init);
-
-// Globals
-
-// Available Levels
-const levels = {
-  easy: 7,
-  medium: 5,
-  hard: 3
-};
-
-// To change level
-const currentLevel = levels.easy;
-
-let time = currentLevel;
-let score = 0;
-let isPlaying;
-
-// DOM Elements
-const wordInput = document.querySelector('#word-input');
-const currentWord = document.querySelector('#current-word');
-const scoreDisplay = document.querySelector('#score');
-const timeDisplay = document.querySelector('#time');
-const message = document.querySelector('#message');
-const seconds = document.querySelector('#seconds');
-const highscoreDisplay = document.querySelector('#highscore');
-
-const words = [
-  'hat',
-  'river',
-  'lucky',
-  'statue',
-  'generate',
-  'stubborn',
-  'cocktail',
-  'runaway',
-  'joke',
-  'developer',
-  'establishment',
-  'hero',
-  'javascript',
-  'nutrition',
-  'revolver',
-  'echo',
-  'siblings',
-  'investigate',
-  'horrendous',
-  'symptom',
-  'laughter',
-  'magic',
-  'master',
-  'space',
-  'definition'
-];
-
-// Initialize Game
-function init() {
-  // Show number of seconds in UI
-  seconds.innerHTML = currentLevel;
-  // Load word from array
-  showWord(words);
-  // Start matching on word input
-  wordInput.addEventListener('input', startMatch);
-  // Call countdown every second
-  setInterval(countdown, 1000);
-  // Check game status
-  setInterval(checkStatus, 50);
-}
-
-// Start match
-function startMatch() {
-  if (matchWords()) {
-    isPlaying = true;
-    time = currentLevel + 1;
-    showWord(words);
-    wordInput.value = '';
-    score++;
-  }
-  
-  // Highscore based on score value for Session Storage
-  if (typeof sessionStorage['highscore'] === 'undefined' || score > sessionStorage['highscore']) {
-    sessionStorage['highscore'] = score;
-  } else {
-    sessionStorage['highscore'] = sessionStorage['highscore'];
-  }
-
-  // Prevent display of High Score: -1
-  if (sessionStorage['highscore'] >= 0) {
-  highscoreDisplay.innerHTML = sessionStorage['highscore'];
-  }
-
-  // If score is -1, display 0
-  if (score === -1) {
-    scoreDisplay.innerHTML = 0;
-  } else {
-    scoreDisplay.innerHTML = score;
-  }
-}
-
-// Match currentWord to wordInput
-function matchWords() {
-  if (wordInput.value === currentWord.innerHTML) {
-    message.innerHTML = 'Correct!!!';
-    return true;
-  } else {
-    message.innerHTML = '';
-    return false;
-  }
-}
-
-// Pick & show random word
-function showWord(words) {
-  // Call an API to get random words
-  fetch('https://random-words-api.vercel.app/word', {
-      method: 'GET',
-      headers: {
-        accept: 'application/json',
-      },
-    }).then(data => data.json()).then((data)=>currentWord.innerHTML = data[0].word)
-  // Output random word
-}
-
-// Countdown timer
-function countdown() {
-  // Make sure time is not run out
-  if (time > 0) {
-    // Decrement
-    time--;
-  } else if (time === 0) {
-    // Game is over
-    isPlaying = false;
-  }
-  // Show time
-  timeDisplay.innerHTML = time;
-}
-
-// Check game status
-function checkStatus() {
-  if (!isPlaying && time === 0) {
-    message.innerHTML = 'Game Over!!!';
-    score = -1;
-  }
-}*/
 window.addEventListener('load', init);
 
 // Globals
@@ -169,40 +26,13 @@ const message = document.querySelector('#message');
 const seconds = document.querySelector('#seconds');
 const highscoreDisplay = document.querySelector('#highscore');
 
-const words = [
-  'hat',
-  'river',
-  'lucky',
-  'statue',
-  'generate',
-  'stubborn',
-  'cocktail',
-  'runaway',
-  'joke',
-  'developer',
-  'establishment',
-  'hero',
-  'javascript',
-  'nutrition',
-  'revolver',
-  'echo',
-  'siblings',
-  'investigate',
-  'horrendous',
-  'symptom',
-  'laughter',
-  'magic',
-  'master',
-  'space',
-  'definition'
-];
 
 // Initialize Game
 function init() {
   // Show number of seconds in UI
   seconds.innerHTML = currentLevel;
   // Load word from array
-  showWord(words);
+  showWord();
   // Start matching on word input
   wordInput.addEventListener('input', startMatch);
   // Call countdown every second
@@ -216,7 +46,7 @@ function startMatch() {
   if (matchWords()) {
     isPlaying = true;
     time = currentLevel + 1;
-    showWord(words);
+    showWord();
     wordInput.value = '';
     score++;
   }
@@ -256,7 +86,7 @@ function matchWords() {
 }
 
 // Pick & show random word
-function showWord(words) {
+function showWord() {
   // Call an API to get random words
   fetch('https://random-words-api.vercel.app/word', {
       method: 'GET',
